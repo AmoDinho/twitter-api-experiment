b33add21f570effd279c5e15198c75c01d7d9bcf
Amo Moloko <u14284783@tuks.co.za>
2018-07-31 15:28:24 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import pic from "./photo1.jpg";
+import pic1 from "./photo2.jpg";
+import "./App.css";
+
+const tweets = [
+
+    {
+        tweetId: 1,
+        title: "Black Panther",
+        author: "Luwdiq Cuda",
+        url: "https://unknown.com",
+        comments: 10,
+        likes: 20,
+        retweets: 50,
+        imgURL: pic
+    },
+    {
+        tweetId: 2,
+        title: "Learn JS for Free",
+        author: "@Darth_Vader",
+        url: "https://deathstar.com",
+        comments: 2,
+        likes: 100,
+        retweets: 800,
+        imgURL: pic1
+
+    }
+
+];
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h1>Twitter Cards</h1>
+                </div>
+                <div className="App-body">
+
+
+                    {tweets.map(item =>(
+                        <div key={item.tweetId}>
+                            <span>{item.title} </span> 
+
+                            <span> {item.author}</span> 
+
+                            <span>{item.comments} </span> 
+
+                            <span>{item.likes} </span> 
+                            <span>{item.retweets} </span> 
+                            <img width="20px" height="20px" src={item.imgURL} />
+                        </div>
+                    ))};
+                </div>
+            </div>
+        );
+    }
+}  
 
 export default App;
